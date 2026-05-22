529dc5ab6c34cb5e56799acf89ba10fb82d74d97
arafah-dhrubo <arafahdhrubo@gmail.com>
2026-05-22 22:37:42 +0600

    feat: setup database configuration

---
+++ src/config/db.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import { Pool } from "pg";
+
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    max: 20,
+    min: 0,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 10000,
+})
+
+export const connectDB = async () => {
+    try {
+        const client = await pool.connect();
+        client.release();
+        console.log("Database connected");
+    } catch (error) {
+        console.error("Database connection failed", error);
+    }
+}
