ad585e9058559e132f05262de70b1b29f755ea55
arafah-dhrubo <arafahdhrubo@gmail.com>
2026-05-22 22:37:42 +0600

    chore: update config and server setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express, { Application, Request, Response } from 'express';
-
+import accountRoutes from "@/modules/account/account.route";
+import issuesRoutes from "@/modules/issues/issues.route";
+import { responseHandler } from './middlewares/response.middleware';
 const app: Application = express();
 
 app.use(express.json());
+app.use(responseHandler);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+app.use('/api/account', accountRoutes);
+app.use('/api/issues', issuesRoutes);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import app from "./app";
+import { connectDB } from "./config/db";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectDB().then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))).catch((error) => console.log(error.message))
