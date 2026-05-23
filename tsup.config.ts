2793ab4f665b3e64c853043d626120ac2c199021
arafah-dhrubo <arafahdhrubo@gmail.com>
2026-05-23 18:40:03 +0600

    fix: update build config and vercel deployment setup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'tsup';
-
 export default defineConfig({
-  entry: ['src/server.ts'],
-  format: ['cjs'],
-  splitting: false,
-  sourcemap: true,
+  entry: ["src/server.ts"],
+  format: ["esm", "cjs"], 
+  target: "esnext",
+  outDir: "dist",
   clean: true,
-  target: 'esnext',
-  outDir: 'dist',
+  bundle: true,
+  splitting: false,
+  sourcemap: true, banner: {
+    js: `
+   import { createRequire } from 'module';
+   const require = createRequire(import.meta.url);
+  `,
+  },
 });
